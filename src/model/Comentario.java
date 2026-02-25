98c7afde65fd1c864c9af63e25d45c1e93263006
Samuca-Surf <samuca.lkm@gmail.com>
2026-02-25 20:09:29 -0300

    Add files via upload

---
+++ src/model/Comentario.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Comentario {
+    private Integer id;
+    private String conteudo;
+    private LocalDateTime criacao;
+    private Usuario usuario;
+    private Postagem postagem;
+
+
+}
---
+++ src/model/Login.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Login {
+    private Usuario usuario;
+    private String token;
+    private Boolean isAutenticado;
+
+    public void cadastrarUsuario(){
+
+    }
+
+    public void Login(){
+
+    }
+
+}
